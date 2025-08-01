f31dcdf4bc23bead3212a55bb385ae5a7161b971
Lucass-Lopes <lucascarletto.rj@gmail.com>
2025-08-01 19:36:49 -0300

    Update Exercício módulo 8.py

--- Exercício módulo 8.py
+++ Exercício módulo 8.py
@@ -29,6 +29,21 @@ df['Qtd_Vendidos'] = df['Qtd_Vendidos'].apply(texto_para_num)
 # Remove valores ausentes e inválidos
 df.dropna(inplace=True)
 
+# Limpa e converte os preços para float
+df['Preço'] = (
+    df['Preço']
+    .astype(str)
+    .str.replace(r'[R$\s]', '', regex=True)
+    .str.replace('.', '', regex=False)
+    .str.replace(',', '.', regex=False)
+    .astype(float)
+)
+
+# Criação de faixas de preço para organizar melhor as filtrações por ele
+bins = [0, 50, 100, 200, 500, 1000, float('inf')]
+labels = ['Até R$50', 'R$50–100', 'R$100–200', 'R$200–500', 'R$500–1000', 'Mais de R$1000']
+df['Faixa_Preco'] = pd.cut(df['Preço'], bins=bins, labels=labels, include_lowest=True)
+
 # Inicializa a aplicação Dash
 app = dash.Dash(__name__)
 app.title = "Análise de E-commerce"
@@ -47,20 +62,20 @@ reg_fig = px.scatter(
     labels={'Nota': 'Nota Média', 'Qtd_Vendidos': 'Quantidade Vendida'}
 )
 
-# Gráfico de Barras: Quantidade Vendida por Categoria
+# Gráfico de Barras: Quantidade Vendida por Preço
 bar_fig = px.bar(
-    df.groupby("Categoria")["Qtd_Vendidos"].sum().reset_index(),
-    x='Categoria', y='Qtd_Vendidos',
-    title='Total de Vendas por Categoria',
-    labels={'Qtd_Vendidos': 'Total Vendido'}
+    df.groupby("Faixa_Preco")["Qtd_Vendidos"].sum().reset_index(),
+    x='Faixa_Preco', y='Qtd_Vendidos',
+    title='Total de Vendas por Faixa de Preço',
+    labels={'Faixa_Preco': 'Faixa de Preço', 'Qtd_Vendidos': 'Total Vendido'}
 )
 
-# Gráfico de Pizza: Participação nas Vendas por Categoria
+# Gráfico de Pizza: Quantidade Vendida por material
 pizza_fig = px.pie(
-    df, names='Categoria', values='Qtd_Vendidos',
-    title='Distribuição de Vendas por Categoria'
+    df.groupby("Material")["Qtd_Vendidos"].sum().reset_index(),
+    names='Material', values='Qtd_Vendidos',
+    title='Distribuição de Vendas por Material'
 )
-
 # Gráfico de Densidade: Distribuição da Nota Média
 density_fig = px.histogram(
     df, x='Nota', nbins=20, marginal='rug',
@@ -82,3 +97,4 @@ app.layout = html.Div([
 # Executa o servidor
 if __name__ == '__main__':
     app.run_server(debug=True)
+
